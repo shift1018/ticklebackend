3d3fde83df6b84134efea59578ec70b393a3eb17
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-11-19 01:53:18 -0500

    make it better

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+
+const router = new Router();
+
+// Register
+
+
+// Login 
+
+
+
+// Get Me
+
+
+
+export default router;
---
+++ routes/users.js
@@ -0,0 +1,7 @@
+// import router Router() from "express".
+
+router.get("/",(req, res) =>{
+    res.send("it's user route");
+});
+
+module.exports = router;
