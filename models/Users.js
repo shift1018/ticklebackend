38cf0001494bd18201702e8351517972ca0e06bf
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-11-22 13:32:22 -0500

    r

---
+++ models/Users.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const UsersSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatarURL: {
+      type: String,
+    },
+    role: [
+      {
+        type: String,
+        required: true,
+        default: "User",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Posts",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comments",
+      },
+    ],
+
+    reactions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Reactions",
+      },
+    ],
+    friendships: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Friendships",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Users", UsersSchema);
+
+// const UsersModel = mongoose.model("Users", UsersSchema);
+// module.exports = UsersModel;
